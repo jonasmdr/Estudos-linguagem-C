b80f782f01dc441e43af0b92a872ea849238a1b9
Jonas Menezes <jonas.mdr.dev@gmail.com>
2026-05-18 14:14:08 -0300

    começando-em-linguagem-c

---
+++ olamundo.c
@@ -0,0 +1,9 @@
+#include <stdio.h> //Inclusão da biblioteca : #include inclui a biblioteca padrão de entrada e saída, necessária para usar a função printf.
+
+//Função principal : int main() é onde o programa começa a execução. Toda aplicação em C começa pela função main.
+int main() {
+    printf("Olá, Mundo!\n");
+    //Saída de dados : A linha printf("Olá, Mundo!\n"); imprime a mensagem "Olá, Mundo!" na tela. O \n adiciona uma nova linha ao final da mensagem.
+    return 0;
+    //Fim do programa : return 0; indica que o programa terminou com sucesso.
+}
