752cd5029fe36b4277bdb20109d3e3248ae9c752
Jonas Menezes <jonas.mdr.dev@gmail.com>
2026-05-21 12:26:37 -0300

    operadores de atribuiçao

---
+++ exercicios/ex004/operadores-de-atribuicao.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+    /*
+    Atribuiçao simples (=)
+    Atribuiçao com soma (+=)
+    Atribuiçao com subtraçao (-=)
+    Atribuiçao com multiplicaçao (*=)
+    Atribuiçao com divisao (/=)
+
+    Os atalhos (como o +=, -=, *= ou /=) só são usados quando você quer atualizar o valor de uma variável usando ela mesma.
+
+    */
+   int num1, num2, resul;
+    num1 = 5;
+    num2 = 7;
+    resul = num1 + num2;
+    resul += 20;
+
+   printf("%d", resul);
+
+
+
+
+}
